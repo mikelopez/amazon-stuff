a99469b8336efca2917cb8241b1e4995070e8723
Marcos Lopez <dev@scidentify.info>
2012-05-21 02:14:49 -0400

    tabfix

--- features/amazon_features.py
+++ features/amazon_features.py
@@ -9,19 +9,19 @@ from AmazonAPI import Amazon
 
 @step(u'Given I instantiate "(.*)"')
 def given_i_instantiate_group1(step, classname):
-	world.set_class = None
-	world.set_class = eval(classname)
-	assert_true(world.set_class)
+  world.set_class = None
+  world.set_class = eval(classname)
+  assert_true(world.set_class)
 
 @step(u'Class should contain "(.*)" method')
 def class_should_contain_group1_method(step, methodname):
-	assert_true(hasattr(world.set_class, methodname))
+  assert_true(hasattr(world.set_class, methodname))
 
 @step(u'Variable "(.*)" should be None')
 def variable_group1_should_be_none(step, var):
-	assert_true(getattr(world.set_class, var) is None)
+  assert_true(getattr(world.set_class, var) is None)
 
 @step(u'Class should contain "(.*)" dict with "(.*)" key')
 def class_should_contain_group1_dict_with_group2_key(step, var, key):
-	assert_true(key in getattr(world.set_class, var).keys())
+  assert_true(key in getattr(world.set_class, var).keys())
 
